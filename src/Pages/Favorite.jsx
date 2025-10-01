b91e505331ae66b66c4d17c0d1acace0952a6743
yasmina77781 <moumenyasmine2004@gmail.com>
2025-10-01 21:28:42 +0100

    Categcarpets

--- src/Pages/Favorite.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import Favoris from "../components/Favoris"
-export default function Favorites() {
-  return <Favoris />;
-}
--- src/Pages/Publish.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import PublishForm from "../components/PublishForm"; // adapte le chemin si besoin
-
-export default function Publish() {
-  return <PublishForm />;
-}
